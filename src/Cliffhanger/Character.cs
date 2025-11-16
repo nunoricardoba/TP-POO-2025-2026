45d686c9d7384fbb15be0962c6e3e5c4b48d47c6
Nuno Ricardo <nricardobaraujo@gmail.com>
2025-11-16 13:08:49 +0000

    Update on class Character

--- src/Cliffhanger/Character.cs
+++ src/Cliffhanger/Character.cs
@@ -1,13 +1,18 @@
 namespace Cliffhanger
 {
-    public class Character : Actor
+    public class Character
     {
         #region Constants
+        const int DefaultId = -1;
+        const string DefaultName = "Unknown";
+        const int StringMaxLength = 50;
         #endregion
 
         #region Attributes
         int characterId;
         string characterName = string.Empty;
+
+        Actor actorWhoPlays;
         #endregion
 
         #region Methods
@@ -30,18 +35,63 @@ namespace Cliffhanger
                 if (CheckName(value)) characterName = value;
             }
         }
+
+        public Actor ActorWhoPlays
+        {
+            get { return actorWhoPlays; }
+        }
         #endregion
 
         #region Constructors
-        public Character() : base()
+        public Character()
         {
             characterId = DefaultId;
             characterName = DefaultName;
+            actorWhoPlays = new Actor();
+        }
+
+        public Character(int id, string name, Actor actorWhoPlays)
+        {
+            if (CheckId(id))
+            {
+                characterId = id;
+            }
+            else
+            {
+                characterId = DefaultId;
+            }
+
+            if (CheckName(name))
+            {
+                characterName = name;
+            }
+            else
+            {
+                characterName = DefaultName;
+            }
+
+            this.actorWhoPlays = actorWhoPlays;
         }
         #endregion
 
         #region Other Methods
-        // depois dar override ao metodo CheckId, para verifocar se está na lista
+        static bool CheckId(int id)
+        {
+            if (id > DefaultId)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static bool CheckName(string name)
+        {
+            if (name.Length < StringMaxLength)
+            {
+                return true;
+            }
+            return false;
+        }
         #endregion
 
         #endregion
