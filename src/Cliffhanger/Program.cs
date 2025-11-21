497f2fe68382acdc8c2cb2a8f87a4aedbf292782
Nuno Ricardo <nricardobaraujo@gmail.com>
2025-11-21 11:09:33 +0000

    Classes Actor and Character deleted

--- src/Cliffhanger/Program.cs
+++ src/Cliffhanger/Program.cs
@@ -6,22 +6,6 @@ namespace Cliffhanger
     {
         static void Main(string[] args)
         {
-            Actor actor1 = new Actor();
-            actor1.Name = "Pedro Pascal";
-            actor1.DateOfBirth = new DateOnly(1975, 4, 2);
-            actor1.Id = 10;
-
-            Actor actor2 = new Actor(11, "Samuel L. Jackson", 1948, 12, 21);
-
-            Character character1 = new Character();
-            character1.CharacterName = "Jules Winnfield";
-            character1.CharacterId = 8;
-            character1.ActorWhoPlays = actor2;
-
-            Actor actor3 = new Actor(7, "Rainn Wilson", 1966, 1, 20);
-
-            Actor.ShowGroupOfActors();
-            Character.ShowgroupOfCharacters();
         }
     }
 }
