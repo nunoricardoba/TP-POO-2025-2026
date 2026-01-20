127da5ed0fcff772b5be8ccf1063cd7d628644a6
Nuno Ricardo Araújo <nricardobaraujo@gmail.com>
2026-01-20 16:10:52 +0000

    Removed edit methods

--- src/BusinessLogic/GlobalRepoControl.cs
+++ src/BusinessLogic/GlobalRepoControl.cs
@@ -63,46 +63,6 @@ namespace BusinessLogic
         }
 
         /// <summary>
-        /// Checks if the element exists, and if it does, tries to edit its attributes.
-        /// </summary>
-        /// <param name="element"></param>
-        /// <param name="dto"></param>
-        /// <returns></returns>
-        public static bool EditElement(T? element, object? dto)
-        {
-            if (!GlobalRepo<T>.DoesElementExist(element))
-                return false;
-
-            return GlobalService.Edit(element, dto);
-        }
-
-        /// <summary>
-        /// Attempts to find an element with the ID passed by parameters.
-        /// If found, tries to edit its attributes.
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="dto"></param>
-        /// <returns></returns>
-        public static bool EditElementById(Guid id, object? dto)
-        {
-            T? element = GlobalRepo<T>.GetElementById(id);
-            return GlobalService.Edit(element, dto);
-        }
-
-        /// <summary>
-        /// Attempts to find an element with the index passed by parameters.
-        /// If found, tries to edit its attributes.
-        /// </summary>
-        /// <param name="index"></param>
-        /// <param name="dto"></param>
-        /// <returns></returns>
-        public static bool EditElementByIndex(int index, object? dto)
-        {
-            T? element = GlobalRepo<T>.GetElementByIndex(index);
-            return GlobalService.Edit(element, dto);
-        }
-
-        /// <summary>
         /// Use the DL layer method to find an element with the ID passed by parameters.
         /// If found, create a clone so that this object is not accessible in the PL layer.
         /// </summary>
